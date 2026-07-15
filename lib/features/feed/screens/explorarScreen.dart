fbcaa99f012af74abdf7bddee7d86bc8ee0455e2
Rafael Mascarenhas Venas <rafavenas@gmail.com>
2026-07-15 17:30:03 -0300

    Add Explorar and Seguindo feed screens
    Add two new feed screens: ExplorarScreen for exploring content and SeguindoScreen for viewing followed content. Both are placeholder implementations that display centered text labels.

---
+++ lib/features/feed/screens/explorarScreen.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class ExplorarScreen extends StatelessWidget {
+  const ExplorarScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Center(child: Text("Tela Explorar"));
+  }
+}
---
+++ lib/features/feed/screens/seguindoScreen.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class SeguindoScreen extends StatelessWidget {
+  const SeguindoScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Center(child: Text("Tela Seguindo"));
+  }
+}
